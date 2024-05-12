fd782fd630c63ae32a1b078686ce84c3bd1f1fcf
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-05-12 21:39:53 +0300

    1st DZ

---
+++ Lesson_3/JS/script.js
@@ -0,0 +1,9 @@
+
+console.log(typeof("Hello"));
+console.log(typeof(123));
+console.log(typeof(true));
+console.log(typeof([1]));
+console.log(typeof(function(){}));
+console.log(typeof(null));
+console.log(typeof(undefined));
+
