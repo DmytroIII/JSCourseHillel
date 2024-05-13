80e2e9d2cf8e6c1aeab0ea226e4e59a2b794d3b0
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-05-13 09:20:34 +0300

    Update script.js

--- Lesson_3/JS/script.js
+++ Lesson_3/JS/script.js
@@ -1,9 +1,18 @@
-
+/*
 console.log(typeof("Hello"));
 console.log(typeof(123));
 console.log(typeof(true));
 console.log(typeof([1]));
 console.log(typeof(function(){}));
 console.log(typeof(null));
-console.log(typeof(undefined));
+console.log(typeof(undefined)); */
+
+
+// Отримання рядків від користувача
+var Input1 = prompt("Введіть перший рядок:");
+var Input2 = prompt("Введіть другий рядок:");
+var Input3 = prompt("Введіть третій рядок:");
+
+// Виведення рядків у довільному порядку
+console.log(`${Input2}\n${Input1}\n${Input3}`);
 
