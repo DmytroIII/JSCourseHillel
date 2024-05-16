988c5decc0860b1f4227b6aa812164d44aa5c953
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-05-16 13:43:55 +0300

    dz3

--- Lesson_4/JS DZ.3/script.js
+++ Lesson_4/JS DZ.3/script.js
@@ -1,21 +1,44 @@
+let yearOfBirth = prompt("Якого ви року народження"); 
+let liveSity = prompt("В якому місті ви живете");
+let kindOfSport = prompt("Улюблений вид спорту"); 
 
+let currentYear = new Date().getFullYear();
+let age = currentYear - yearOfBirth;
 
-/* var userName = prompt("Ім'я користувача");
-alert(`Hello, ${userName}! How are you?`); */
-
-/* var userName = prompt("Введіть трьохзначне число");
-alert(`Hello, ${userName}! How are you?`); */
-
-let theSameNumber = (inputNumber) => {
-    let array = inputNumber.toString().split("");
-
-    if (array[0] === array[1] && array[1] === array[2]) {
-       console.log("Числа однакові")
-    } else {
-        console.log("Числа НЕ однакові")
-    }
-
+if (yearOfBirth === null) {
+    age = "Шкода що не захотіли вводити дату народження"
 }
 
-theSameNumber(345);
-theSameNumber(333);
+let message;
+
+if (liveSity === "Київ") {
+    message = "Ти живеш у столиці України";
+} else if (liveSity === "Вашингтон") {
+    message = "Ти живеш у столиці США";
+} else if (liveSity === "Лондон") {
+    message = "Ти живеш у столиці Великобританії";
+} 
+else if (liveSity === null) {
+    message = "Шкода що не захотіли вводити місто в якому живете";
+}
+else {
+    message = `Ти живеш у місті ${liveSity}`;
+}
+
+let sportLeader;
+
+if (kindOfSport === "Баскетбол") {
+    sportLeader = "Круто! Хочеш стати Джорданом?";
+} else if (kindOfSport === "Футбол") {
+    sportLeader = "Круто! Хочеш стати Роналду?";
+} else if (kindOfSport === "Формула-1") {
+    sportLeader = "Круто! Хочеш стати Хемілтоном?";
+} 
+ else if (kindOfSport === null) {
+    sportLeader = "Шкода що не захотіли вводити улюблений вид спорту";
+}
+else {
+    sportLeader = ``;
+}
+
+alert("Ваш вік: " + age + " " + message + " " + sportLeader);
