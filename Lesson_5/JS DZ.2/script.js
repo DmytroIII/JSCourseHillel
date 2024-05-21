37ea4200728ca7b05eb87cc117070bed6ac0d687
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-05-21 23:16:15 +0300

    course

--- Lesson_5/JS DZ.2/script.js
+++ Lesson_5/JS DZ.2/script.js
@@ -1,24 +1,7 @@
 
-let theSameNumber = (inputNumber) => {
-    let array = inputNumber.toString().split("");
-    if (array[0] === array[1] && array[1] === array[2]) {
-       console.log("Числа всі однакові")
-    } else {
-        console.log("Числа не всі однакові")
-    }
-} 
 
-theSameNumber(545);
-theSameNumber(777);
+var COURSE = 26;
 
-let theSameNumberAll = (inputNumber) => {
-    let array = inputNumber.toString().split("");
-    if (array[0] === array[1] || array[1] === array[2] || array[0] === array[2]) {
-       console.log("Е числа однакові")
-    } else {
-        console.log("Немає однакових чисел")
-    }
+for (let i=10; i<=100; i+=10) {
+  console.log(`${i} доларів коштує ${i*COURSE}`)
 }
-
-theSameNumberAll(545);
-theSameNumberAll(356);
