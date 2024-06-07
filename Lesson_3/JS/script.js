179e78579f71abb520c384f170c6aada68c01afe
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-06-08 00:40:23 +0300

    jn
    jn

--- Lesson_3/JS/script.js
+++ Lesson_3/JS/script.js
@@ -1,11 +1,27 @@
+console.log(typeof("Hello"));
+/*
+function removeElement(array) {
+   for (let i=1; i=array.length; i++){
+    console.log(i);
+   }
+}
+console.log(removeElement([1, 3, 4, 6, 2, 5, 7]));
 
+<<<<<<< Updated upstream
 console.log(typeof"Hello");
+=======
+
+
+/*
+console.log(typeof("Hello"));
+>>>>>>> Stashed changes
 console.log(typeof(123));
 console.log(typeof(true));
 console.log(typeof([1]));
 console.log(typeof(function(){}));
 console.log(typeof(null));
 
+<<<<<<< Updated upstream
 /*
 // Отримання рядків від користувача
 let Input1 = prompt("Введіть перший рядок:");
@@ -26,3 +42,18 @@ function cnangeNumber(inputNumber) {
     return inputNumber.toString().split("").join(" ");
 };
 console.log(cnangeNumber(12345)); */
+=======
+let input1 = prompt("Введіть перший рядок:");
+let input2 = prompt("Введіть другий рядок:");
+let input3 = prompt("Введіть третій рядок:");
+
+// Створюємо масив з отриманими рядками
+let strings = [input1, input2, input3];
+
+/* Виводимо рядки у довільному порядку за допомогою шаблонних рядків
+console.log(`${strings[Math.floor(Math.random() * strings.length)]}`);
+console.log(`${strings[Math.floor(Math.random() * strings.length)]}`);
+console.log(`${strings[Math.floor(Math.random() * strings.length)]}`); */
+
+
+>>>>>>> Stashed changes
