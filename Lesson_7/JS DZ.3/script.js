63cb0441f76453f61d36427b0cf3abcc2a19de62
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-06-09 18:55:31 +0300

    dz3
    dz3

--- Lesson_7/JS DZ.3/script.js
+++ Lesson_7/JS DZ.3/script.js
@@ -1,9 +1,22 @@
+function promptForNumber() {
+  let iteration = 0;
+  let userInput;
 
-function removeElement(array, item) {
-     for (let i=0; i<array.length; i++){
-      if (array[i] === item) {
-        array.splice(i,1)
+  while (iteration < 10) {
+      userInput = prompt("Введіть число більше 100:");
+      
+      if (userInput > 100) {
+          console.logs(`Ви ввели число більше 100: ${userInput}`);
+          return;
       }
-     }
-     return array;
-}  
+
+      iteration++;
+
+      if (iteration === 10) {
+          console.log(`Ви досягли максимальну кількість спроб. Останнє введене число: ${userInput}`);
+          return;
+      }
+  }
+}
+
+promptForNumber();
