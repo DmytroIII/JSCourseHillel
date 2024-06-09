c7d98a3c2be00d2ea7bdafebd5aa7baa667703a4
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-06-09 17:34:40 +0300

    dz7
    dz7

--- Lesson_7/JS DZ.2/script.js
+++ Lesson_7/JS DZ.2/script.js
@@ -1,16 +1,9 @@
 
 
-function countAverege (array) {
-  const numberArray = [];
-  for (let i=0; i< array.length; i+=1) {
-    if (typeof array[i] === "number") {
-      numberArray.push(array[i]);
-    }
-  }
-
-  let summ = 0;
-  for (let i=0; i<numberArray.length; i++){
-    summ += numberArray[i];
-  }
-  return summ/numberArray.length;
+function name(a) {
+  return function(b) {
+      return a * b;
+  };
 }
+
+console.log(name(3)(5));
