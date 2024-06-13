85fb2ff1829ede978b2b43052cb9f5575e4b342a
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-06-13 17:03:46 +0300

    dz
    dz

--- Lesson_10/JS DZ.1/script.js
+++ Lesson_10/JS DZ.1/script.js
@@ -1,3 +1,37 @@
 
 
-console.log("Helo")
+
+const user =  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+},
+getUserInfo() {
+    return {
+      name: this.name,
+      username: this.username,
+      email: this.email
+    };
+  },
+
+displayUserInfo() {
+    const info = this.getUserInfo();
+    for (const key in info) {
+        console.log(`${key}: ${info[key]}`);
+      }
+  }
+
+}
+
+user.displayUserInfo();
+
